46d4a3cd49cdf426c2ce5702ead41af7d3be0cf9
ilammy <a.lozovsky@gmail.com>
2019-03-15 15:54:05 +0200

    add target and scheme for macOS

---
+++ Frameworks/macos/OpenSSL.h
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpenSSL.
+FOUNDATION_EXPORT double OpenSSLVersionNumber;
+
+//! Project version string for OpenSSL.
+FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
+
+// Public headers:
