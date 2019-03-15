4ff58a97be85e999e26c17731f8a858abfba416a
ilammy <a.lozovsky@gmail.com>
2019-03-15 15:23:11 +0200

    proper modular headers

--- Frameworks/ios/OpenSSL.h
+++ Frameworks/ios/OpenSSL.h
@@ -6,4 +6,4 @@ FOUNDATION_EXPORT double OpenSSLVersionNumber;
 //! Project version string for OpenSSL.
 FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <OpenSSL__iOS_/PublicHeader.h>
+// Public headers:
