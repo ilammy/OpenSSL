c3a90a01e26829a5953eb2adf90898f258e466ca
ilammy <a.lozovsky@gmail.com>
2019-03-15 16:20:19 +0200

    rename frameworks to lowercase openssl in order to have headers under expected paths (so that apps don't have to fixup their #include <openssl/evp.h> lines)
    also drop magic script steps, just hardcode correct files

--- Frameworks/ios/OpenSSL.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for OpenSSL.
-FOUNDATION_EXPORT double OpenSSLVersionNumber;
-
-//! Project version string for OpenSSL.
-FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
-
-// Public headers:
--- Frameworks/macos/OpenSSL.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for OpenSSL.
-FOUNDATION_EXPORT double OpenSSLVersionNumber;
-
-//! Project version string for OpenSSL.
-FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
-
-// Public headers:
