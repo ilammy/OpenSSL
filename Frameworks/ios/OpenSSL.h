d67bd7891bb156943033bdd7aebfd062375fcda7
ilammy <a.lozovsky@gmail.com>
2019-03-15 15:06:50 +0200

    add an actual *framework* target to xcode

---
+++ Frameworks/ios/OpenSSL.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenSSL.
+FOUNDATION_EXPORT double OpenSSLVersionNumber;
+
+//! Project version string for OpenSSL.
+FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenSSL__iOS_/PublicHeader.h>
